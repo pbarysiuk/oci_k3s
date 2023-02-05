8a09c75c03f82314c25d81d9e2deed56d92d81ee
Pavel Barysiuk <pavel_barysiuk@epam.com>
2023-02-05 13:40:31 +0100

    vars fix

--- modules/oci/compute/vars.tf
+++ modules/oci/compute/vars.tf
@@ -43,7 +43,7 @@ variable "fault_domains" {
 
 variable "PATH_TO_PUBLIC_KEY" {
   type        = string
-  default     = "~/.ssh/id_rsa.pub"
+  default     = "./id_rsa.pub"
   description = "Path to your public key"
 }
 
